35f78c1fff27a2c76100cd6f673d9400a519bdb4
victorsidedev <victorside.dev@gmail.com>
2026-09-09 22:42:51 -0500

    allowd cors origin for frontend and rest methods

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.purchase import Purchase
 from models.item import Item
@@ -8,6 +9,13 @@ from routes.purchase_routes import purchase_router
 from routes.sale_routes import sale_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://flipup-frontend.onrender.com"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type"],
+)
 
 app.include_router(purchase_router)
 app.include_router(sale_router)
